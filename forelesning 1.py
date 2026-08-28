7e6fa7ca0192bcd912c4cd4c2caea2592736837e
dinamariaoien <dinamariaoien@gmail.com>
2026-08-28 12:06:40 +0200

    uke 35
    alt av øving og lring fra uke 35

---
+++ forelesning 1.py
@@ -0,0 +1,39 @@
+# Dynamisk data
+print ("I am new")
+name= input("Enter your name:")
+print ("Hello" + name)
+
+firstname= input("Enter your first name:")
+secondname= input("Enter your second name:")
+print(firstname + " " + secondname)
+
+# Statisk data
+print ("name:", "Sara", "Age:", "25")
+print("My name is Dina\nI am 24 years old\nI live in Oslo") # /n deler opp i linjer
+
+print("*" * 30) # Multiplisere
+print("Name:", "Sara")
+print("Age:", "25")
+print("*" * 30)
+
+# Øve
+name= input("What is your name?")
+age= input("What is your age?")
+adress= input("Where do you live?")
+print("Hello " + name + "!" + " " + "You are " + age + " " + "years old and live in " + adress +".")
+
+firstnumber= input("First number:")
+secondnumber= input("Second number:")
+sum = firstnumber + secondnumber
+print(sum)
+difference = firstnumber - secondnumber
+print(difference)
+multiplication = firstnumber * secondnumber
+print(multiplication)
+
+
+
+
+
+
+
