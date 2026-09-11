67d3adde78573f66e778e499d5ff57c2a0c20b6c
dinamariaoien <dinamariaoien@gmail.com>
2026-09-11 11:59:18 +0200

    Add calculator module fra fredag uke 37 sine oppgaver

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# FREDAG uke 37
+# Task 10
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
+
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+
+def subtract(a, b):
+    """Subtract one number from another."""
+    return a - b
+
+
+def divide(a, b):
+    """Divide one number by another."""
+    return a / b
+
+if __name__ == "__main__":
+    print(add(10, 5))
+    print(subtract(10, 5))
+    print(multiply(10, 5))
+    print(divide(10, 5))
